2762cde4a6bf812d574a6909a15c2ab1818535af
BasselSh <basselalshawareb1@gmail.com>
2025-03-03 23:36:17 +0300

    Script to measure inference time

---
+++ inference.py
@@ -0,0 +1,33 @@
+
+import torch
+import pandas as pd
+
+from oml import datasets as d
+from oml.inference import inference
+from oml.metrics import calc_retrieval_metrics_rr
+
+from oml.models import ViTExtractor
+from oml.registry import get_transforms_for_pretrained
+from oml.retrieval import RetrievalResults, AdaptiveThresholding
+import time
+from torch.utils.data import DataLoader
+device = "cuda"
+
+model = ViTExtractor.from_pretrained("vits16_dino")
+state_dict = torch.load("model.pth", map_location="cpu")
+model.load_state_dict(state_dict)
+model = model.to(device).eval()
+
+transform, _ = get_transforms_for_pretrained("vits16_dino")
+
+df_test = pd.read_csv("val.csv")
+test = d.ImageQueryGalleryLabeledDataset(df_test, transform=transform)
+input_tensor_key = test.input_tensors_key
+loader = DataLoader(dataset=test, batch_size=1, num_workers=1, shuffle=False)
+batch= next(iter(loader))
+img = batch[input_tensor_key].to(device)
+# print(img.shape)
+start = time.time()
+embeddings = model(img)
+end = time.time()
+print(f"Time taken: {end - start}")
