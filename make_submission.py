0ee46617157a644b16232f40dad78652ac690859
BasselSh <basselalshawareb1@gmail.com>
2025-03-07 00:07:28 +0300

    Make submission with custom model

--- make_submission.py
+++ make_submission.py
@@ -8,7 +8,14 @@ from oml.inference import inference
 
 from oml.models import ViTExtractor
 from oml.registry import get_transforms_for_pretrained
+import torch.nn as nn
+from facenet_pytorch import MTCNN, InceptionResnetV1
+import torch.nn as nn
+from facenet_pytorch import fixed_image_standardization
+from torchvision import transforms
+import numpy as np
 
+device = "cuda"
 
 def create_sample_sub(pair_ids: List[str], sim_scores: List[float]):
     sub_sim_column = "similarity"
@@ -16,16 +23,43 @@ def create_sample_sub(pair_ids: List[str], sim_scores: List[float]):
     return pd.DataFrame({id_column: pair_ids, sub_sim_column: sim_scores})
 
 
+
+def get_facenet():
+    model = InceptionResnetV1(
+    classify=True,
+    pretrained='vggface2',
+    num_classes=10
+    ).to(device)
+    model.dropout = nn.Identity()
+    model.last_linear = nn.Identity()
+    model.last_bn = nn.Identity()
+    model.logits = nn.Identity()
+    return model
+
+def get_transforms_facenet():
+    return transforms.Compose([
+        np.float32,
+        transforms.ToTensor(),
+        fixed_image_standardization
+    ])
+models_dict = {
+    "vits16_dino": {"model": ViTExtractor.from_pretrained("vits16_dino").to(device).train(), "transform": get_transforms_for_pretrained("vits16_dino")[0]},
+    "facenet": {"model": get_facenet(), "transform": get_transforms_facenet()}
+}
+
+
+
 if __name__ == "__main__":
     if not os.path.exists("data"):
         os.makedirs("data")
-
-    device = "cpu"
+        
     test_path = "test.csv"
 
-    model = ViTExtractor.from_pretrained("vits16_dino")
-    state_dict = torch.load("model.pth", map_location="cpu")
-    model.load_state_dict(state_dict)
+    model_name = "facenet"
+    model_dict = models_dict[model_name]
+    model = model_dict["model"]
+    transform = model_dict["transform"]
+    model.load_state_dict(torch.load("model.pth"))
     model = model.to(device).eval()
 
     transform, _ = get_transforms_for_pretrained("vits16_dino")
