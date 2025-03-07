8e447e87d6829be1cb58bf81ef77f77e99f8e1ba
BasselSh <basselalshawareb1@gmail.com>
2025-03-07 23:04:23 +0300

    sample a batch with albumentations transforms

--- transforms_test.py
+++ transforms_test.py
@@ -3,17 +3,15 @@ from torch.utils.data import DataLoader
 import albumentations as A
 from torchvision.utils import make_grid
 import matplotlib.pyplot as plt
-from albumentations.pytorch import ToTensorV2
-
+import numpy as np
 transforms = A.Compose([
     A.Resize(224, 224),
-    FaceCutOut(landmarks_df="points_df_with_img_index.csv", p=0.5),
+    # FaceCutOut(landmarks_df="points_df_with_img_index.csv", p=0.5),
     # A.ToGray(p=0.2),
     A.HorizontalFlip(p=0.5),
-    ToTensorV2()
 ])
 
-train_dataset = RealFakeTripletDataset(root_dir="data/train/images", meta_path="data/train/meta.json", transforms_ablu=transforms)
+train_dataset = RealFakeTripletDataset(root_dir="data/train/images", meta_path="data/train/meta.json", transforms_albu=transforms)
 
 dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 
