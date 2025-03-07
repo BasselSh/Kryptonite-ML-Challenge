0a2fa5990d817ad1ddf72e230cf0ead0da01e3d8
BasselSh <basselalshawareb1@gmail.com>
2025-03-07 23:03:47 +0300

    Automatic model loading from last working dit

--- make_submission.py
+++ make_submission.py
@@ -14,8 +14,15 @@ import torch.nn as nn
 from facenet_pytorch import fixed_image_standardization
 from torchvision import transforms
 import numpy as np
+import argparse
 
 device = "cuda"
+OUTPUT_DIR = "output"
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model", type=str)
+    return parser.parse_args()
+
 
 def create_sample_sub(pair_ids: List[str], sim_scores: List[float]):
     sub_sim_column = "similarity"
@@ -50,20 +57,20 @@ models_dict = {
 
 
 if __name__ == "__main__":
-    if not os.path.exists("data"):
-        os.makedirs("data")
-        
-    test_path = "test.csv"
+    args = parse_args()
+    model_name = args.model
+    last_work_dir_path_file = "last_work_dir.txt"
+    last_work_dir_path = open(last_work_dir_path_file, 'r').read()
+    model_path = os.path.join(last_work_dir_path, "best_model.pth")
+    submission_path = os.path.join(last_work_dir_path, "submission.csv")
 
-    model_name = "facenet"
     model_dict = models_dict[model_name]
     model = model_dict["model"]
     transform = model_dict["transform"]
-    model.load_state_dict(torch.load("model.pth"))
+    model.load_state_dict(torch.load(model_path))
     model = model.to(device).eval()
 
-    transform, _ = get_transforms_for_pretrained("vits16_dino")
-
+    test_path = "test.csv"
     df_test = pd.read_csv(test_path)
     test = d.ImageQueryGalleryLabeledDataset(df_test, transform=transform)
     embeddings = inference(model, test, batch_size=32, num_workers=0, verbose=True)
@@ -76,4 +83,4 @@ if __name__ == "__main__":
     pair_ids = pair_ids[::2]
 
     sub_df = create_sample_sub(pair_ids, sim_scores)
-    sub_df.to_csv("data/submission.csv", index=False)
+    sub_df.to_csv(submission_path, index=False)
